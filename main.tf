0b62c8a00006d443635241474f35ab240ecd5ad8
Javier Moreno <javiermorenodp@gmail.com>
2026-05-04 23:40:11 +0200

    Fix: provider AWS correctamente configurado para LocalStack

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = ">= 1.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -8,10 +10,14 @@ terraform {
 }
 
 provider "aws" {
-  region            = "us-east-1"
-  access_key        = "test"
-  secret_key        = "test"
-  s3_use_path_style = true
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  s3_use_path_style           = true
+
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
 
   endpoints {
     s3 = "http://localhost:4566"
@@ -19,9 +25,7 @@ provider "aws" {
 }
 
 module "mi_recurso" {
-  source = "./modules/mi-recurso"
-
+  source = "./modules/mi_recurso"
   nombre_bucket = "mi-bucket-localstack"
 }
-
 
